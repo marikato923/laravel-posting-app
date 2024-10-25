f234430833b0c7d7ef289387de25ee8128114b52
MariKato <marikato923@gmail.com>
2024-10-25 23:09:20 +0900

    課題「Twitter風アプリに機能を追加しよう」

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Iluminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 class User extends Authenticatable implements AuthMustVerifyEmail 
 {
